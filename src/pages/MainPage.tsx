730bafa7c8c0f6b83184f7139cfebcf4b7a28e22
ephedrini <ephedrini@yandex.ru>
2025-04-14 23:40:52 +0300

    feat(mainpage): add sections

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,58 +1,23 @@
-import { Search2Icon } from '@chakra-ui/icons';
-import {
-    Box,
-    Flex,
-    FormControl,
-    FormLabel,
-    Heading,
-    IconButton,
-    Input,
-    InputGroup,
-    InputRightElement,
-    Select,
-    Switch,
-} from '@chakra-ui/react';
+import { Box, Flex } from '@chakra-ui/react';
 
-import { NewCardList } from '~/components/CardList/NewRecipeCardList';
+import { HeaderConatainer } from '~/components/headerContainer';
+import { MostPopularSection } from '~/components/mostPopularSection';
+import { RelevantSectionLarge } from '~/components/relevantSectionLarge';
+import { BlogsSection } from '~/components/Sections/blogsSection';
+import { SliderSection } from '~/components/sliderSection';
 
 export function MainPage() {
     return (
-        <Box w={1360} m='0 auto' py={5}>
-            <Flex justify='center' flexDirection='column' w={520} margin='0 auto'>
-                <Flex flexDirection='column' justifyContent='space-around' gap='25px'>
-                    <Heading as='h1' fontSize='50px'>
-                        Приятного аппетита!
-                    </Heading>
-                    <Flex gap={3}>
-                        <IconButton size='lg' aria-label='' />
-                        <InputGroup>
-                            <Input size='lg' type='search' placeholder='Название или ингридиент' />
-                            <InputRightElement>
-                                <Search2Icon color='gray.300' />
-                            </InputRightElement>
-                        </InputGroup>
-                    </Flex>
-                    <Flex>
-                        <FormControl display='flex' alignItems='center'>
-                            <FormLabel htmlFor='alerts' mb='0'>
-                                Исключить мои аллергены
-                            </FormLabel>
-                            <Switch id='alerts' />
-                        </FormControl>
-                        <Select placeholder='Select option'>
-                            <option value='option1'>Option 1</option>
-                            <option value='option2'>Option 2</option>
-                            <option value='option3'>Option 3</option>
-                        </Select>
-                    </Flex>
+        <Box className='mainpage' m='0 auto' pt='100px' px={4}>
+            <Flex flexDirection='column' gap={10}>
+                <Flex justify='center' align='center' flexDirection='column'>
+                    <HeaderConatainer />
                 </Flex>
+                <SliderSection />
+                <MostPopularSection />
+                <BlogsSection />
+                <RelevantSectionLarge />
             </Flex>
-            <Box mt='45px'>
-                <Heading as='h2' textAlign='left' fontSize='5xl' fontWeight='medium' mb='20px'>
-                    Новые рецепты
-                </Heading>
-                <NewCardList />
-            </Box>
         </Box>
     );
 }
