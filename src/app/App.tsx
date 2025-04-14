f5d083869d9406eb7fcfc1bb2ab2248787c27173
ephedrini <ephedrini@yandex.ru>
2025-04-14 23:42:01 +0300

    feat(*): commit app

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,30 +1,18 @@
 import './App.css';
 
-import { Box, ChakraProvider, Flex } from '@chakra-ui/react';
-import { useNavigate } from 'react-router';
+import { ChakraProvider } from '@chakra-ui/react';
 
-import { Aside } from '~/components/aside';
-import { AsideLeft } from '~/components/asideLeft';
-import { Header } from '~/components/Header';
-import { AppRoutes } from '~/components/route';
-import theme from '~/components/theming/theme';
+import { GridLayout } from '~/components/grid';
 import { useGetPostsQuery } from '~/query/services/posts.ts';
+import theme from '~/UI-kit/theme';
 
 function App() {
     const { data: _data, isLoading: _isLoading } = useGetPostsQuery();
-    const navigate = useNavigate();
+
     return (
         <>
             <ChakraProvider theme={theme}>
-                <Box>
-                    <Header onClick={() => navigate('/')} />
-                    <Flex>
-                        <AsideLeft />
-
-                        <AppRoutes />
-                        <Aside />
-                    </Flex>
-                </Box>
+                <GridLayout />
             </ChakraProvider>
         </>
     );
