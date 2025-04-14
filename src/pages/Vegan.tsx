b0c538bb043fcc8297a81a72ab746ad8346f076e
ephedrini <ephedrini@yandex.ru>
2025-04-14 23:26:25 +0300

    feat(vegan): add vegan info

--- src/pages/Vegan.tsx
+++ src/pages/Vegan.tsx
@@ -1,5 +1,27 @@
-import React from 'react';
+import { Box, Flex, Tab, TabList, Tabs } from '@chakra-ui/react';
+
+import { VeganPageCardList } from '~/components/CardList/VeganCardList';
+import { HeaderConatainer } from '~/components/headerContainer';
 
 export function Vegan() {
-    return <div>vegan</div>;
+    return (
+        <Box className='vegan' m='0 auto' pt='100px'>
+            <Flex justify='center' flexDirection='column' align='center' gap='20px'>
+                <HeaderConatainer />
+            </Flex>
+            <Tabs variant='customTabs'>
+                <TabList fontSize='sm'>
+                    <Tab>Закуски</Tab>
+                    <Tab>Первые блюда</Tab>
+                    <Tab>Вторые блюда</Tab>
+                    <Tab>Гарниры</Tab>
+                    <Tab>Десерты</Tab>
+                    <Tab>Выпечка</Tab>
+                    <Tab>Сыроедческие блюда</Tab>
+                    <Tab>Напитки</Tab>
+                </TabList>
+            </Tabs>
+            <VeganPageCardList />
+        </Box>
+    );
 }
