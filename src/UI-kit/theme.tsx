ce177608ff469736786c30706a510c310210251e
ephedrini <ephedrini@yandex.ru>
2025-04-14 23:39:40 +0300

    feat(styles): add styles

--- src/UI-kit/theme.tsx
+++ src/UI-kit/theme.tsx
@@ -2,15 +2,18 @@ import { extendTheme } from '@chakra-ui/react';
 
 import { accordionTheme } from './accordionTheme';
 import { buttonTheme } from './button';
+import { inputTheme } from './input';
 import { switchTheme } from './switch';
+import { tabsTheme } from './tabs';
 
 const theme = extendTheme({
     colors: {
         lime: {
             50: '#D7FF94',
             150: '#D7FF94',
-            500: 'C4FF61',
-            900: 'C4FF61',
+            500: '#C4FF61',
+            800: '#134B00',
+            900: '#134B00',
         },
     },
 
@@ -18,6 +21,8 @@ const theme = extendTheme({
         Accordion: accordionTheme,
         Button: buttonTheme,
         Switch: switchTheme,
+        Input: inputTheme,
+        Tabs: tabsTheme,
     },
 });
 
