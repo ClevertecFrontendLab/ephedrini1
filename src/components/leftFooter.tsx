652010491c234bcd49255cd340097032f1cb8855
ephedrini <ephedrini@yandex.ru>
2025-04-14 23:40:15 +0300

    feat(footer): add link

--- src/components/leftFooter.tsx
+++ src/components/leftFooter.tsx
@@ -1,7 +1,9 @@
-import { Box, Text } from '@chakra-ui/react';
+import { Box, Link, Text } from '@chakra-ui/react';
+
+import { ExitIcon } from './customIcons/exitIcon';
 
 export const LeftFooter: React.FC = () => (
-    <Box w='280px'>
+    <Box as='footer' w='280px' data-test-id='footer'>
         <Box textAlign='left' w='200px' pl='25px'>
             <Text fontSize='12px' opacity='0.24'>
                 Версия программы 03.25
@@ -9,6 +11,9 @@ export const LeftFooter: React.FC = () => (
             <Text fontSize='12px'>
                 Все права защищены, ученический файл, ©Клевер Технолоджи, 2025
             </Text>
+            <Link href='https://chakra-ui.com' isExternal>
+                <ExitIcon /> выйти
+            </Link>
         </Box>
     </Box>
 );
